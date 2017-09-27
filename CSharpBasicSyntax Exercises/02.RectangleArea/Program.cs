4b59e964c84ee4cb18e1507e4921232ada742186
RadoslavStanchev <draconissickle@gmail.com>
2017-09-27 15:32:17 +0300

    Added first Homework from first Exercise

---
+++ CSharpBasicSyntax Exercises/02.RectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width = float.Parse(Console.ReadLine());
+            float height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine($"{area:F2}");
+        }
+    }
+}
---
+++ CSharpBasicSyntax Exercises/04.BeverageLabels/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energy = double.Parse(Console.ReadLine());
+            double sugar = double.Parse(Console.ReadLine());
+
+            double energyContent = volume / 100 * energy;
+            double sugarContent = volume / 100 * sugar;
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energyContent}kcal, {sugarContent}g sugars");
+        }
+    }
+}
